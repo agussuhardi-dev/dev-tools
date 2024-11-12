cf041ce20d6a785d0fccf7f3070a6addc633a4ae
agussuhardi <agussuhardi.dev@gmail.com>
2024-11-12 07:40:29 +0700

    number convert

--- src/app/routes/converts/number-base/number-base.component.ts
+++ src/app/routes/converts/number-base/number-base.component.ts
@@ -1,18 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import { PageHeaderComponent } from '@shared';
+import { BreadcrumbComponent, PageHeaderComponent } from '@shared';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-converts-number-base',
   templateUrl: './number-base.component.html',
   styleUrl: './number-base.component.scss',
   standalone: true,
-  imports: [PageHeaderComponent]
+  imports: [
+    PageHeaderComponent,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTabsModule,
+    FormsModule,
+    NgIf,
+    NgFor,
+    BreadcrumbComponent,
+  ],
 })
 export class ConvertsNumberBaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  get binValue(): string {
+    return this._binValue;
   }
 
+  set binValue(value: string) {
+    this._binValue = value;
+  }
+  decimalValue = '';
+  hexValue = '';
+  octValue = '';
+  private _binValue = '';
+
+  ngOnInit(): void {
+  }
+
+  convertDecimal(): void {
+    const decimal = parseInt(this.decimalValue, 10);
+    this.hexValue = decimal.toString(16);
+    this.octValue = decimal.toString(8);
+    this._binValue = decimal.toString(2);
+  }
+
+  convertHex(): void {
+    const hex = parseInt(this.hexValue, 16);
+    this.decimalValue = hex.toString(10);
+    this.octValue = hex.toString(8);
+    this._binValue = hex.toString(2);
+  }
+
+  convertOct(): void {
+    const oct = parseInt(this.octValue, 8);
+    this.decimalValue = oct.toString(10);
+    this.hexValue = oct.toString(16);
+    this._binValue = oct.toString(2);
+  }
+
+  convertBin(): void {
+    const bin = parseInt(this._binValue, 2);
+    this.decimalValue = bin.toString(10);
+    this.hexValue = bin.toString(16);
+    this.octValue = bin.toString(8);
+  }
 }
