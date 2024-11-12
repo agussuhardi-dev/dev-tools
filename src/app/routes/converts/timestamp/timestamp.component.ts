7d1e0ce3fc7d076c1488eecaffbe2df7835d5f5e
agussuhardi <agussuhardi.dev@gmail.com>
2024-11-12 07:11:38 +0700

    timestamp converts

--- src/app/routes/converts/timestamp/timestamp.component.ts
+++ src/app/routes/converts/timestamp/timestamp.component.ts
@@ -1,18 +1,140 @@
 import { Component, OnInit } from '@angular/core';
-import { PageHeaderComponent } from '@shared';
+import { BreadcrumbComponent, PageHeaderComponent } from '@shared';
+import { MatCard, MatCardContent, MatCardModule } from '@angular/material/card';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle,
+} from '@angular/material/datepicker';
+import {
+  MatAccordion,
+  MatExpansionModule,
+  MatExpansionPanel,
+  MatExpansionPanelTitle,
+} from '@angular/material/expansion';
+import { MatList, MatListItem, MatListModule } from '@angular/material/list';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgForOf, NgIf } from '@angular/common';
+import { MatNativeDateModule, MatOption } from '@angular/material/core';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { MatButton } from '@angular/material/button';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-converts-timestamp',
   templateUrl: './timestamp.component.html',
   styleUrl: './timestamp.component.scss',
   standalone: true,
-  imports: [PageHeaderComponent]
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatListModule,
+    FormsModule,
+    NgIf,
+    NgFor,
+    BreadcrumbComponent,
+    MatTabGroup,
+    MatTab,
+    MatButton,
+    MatSelect,
+    MatOption,
+  ],
 })
 export class ConvertsTimestampComponent implements OnInit {
-
-  constructor() { }
+  timeInfo: any = {};
+  inputDatetime: string = '';
+  inputEpoch: number | null = null;
+  selectedTimeZone: any;
+  timeZones: any[] = [];
+  errorMessage: string = '';
 
   ngOnInit() {
+    this.timeZones = [
+      { name: 'UTC-12:00 (Baker Island Time)', offset: 'Etc/GMT+12' },
+      { name: 'UTC-11:00 (Samoa Time)', offset: 'Etc/GMT+11' },
+      { name: 'UTC-10:00 (Hawaii-Aleutian Time)', offset: 'Etc/GMT+10' },
+      { name: 'UTC-09:00 (Alaska Time)', offset: 'Etc/GMT+9' },
+      { name: 'UTC-08:00 (Pacific Time)', offset: 'Etc/GMT+8' },
+      { name: 'UTC-07:00 (Mountain Time)', offset: 'Etc/GMT+7' },
+      { name: 'UTC-06:00 (Central Time)', offset: 'Etc/GMT+6' },
+      { name: 'UTC-05:00 (Eastern Time)', offset: 'Etc/GMT+5' },
+      { name: 'UTC-04:00 (Atlantic Time)', offset: 'Etc/GMT+4' },
+      { name: 'UTC-03:00 (West Africa Time)', offset: 'Etc/GMT+3' },
+      { name: 'UTC-02:00 (Mid-Atlantic Time)', offset: 'Etc/GMT+2' },
+      { name: 'UTC-01:00 (Greenwich Mean Time)', offset: 'Etc/GMT+1' },
+      { name: 'UTC+00:00 (Coordinated Universal Time)', offset: 'Etc/GMT' },
+      { name: 'UTC+01:00 (Central European Time)', offset: 'Etc/GMT-1' },
+      { name: 'UTC+02:00 (Eastern European Time)', offset: 'Etc/GMT-2' },
+      { name: 'UTC+03:00 (Moscow Time)', offset: 'Etc/GMT-3' },
+      { name: 'UTC+04:00 (Astrakhan Time)', offset: 'Etc/GMT-4' },
+      { name: 'UTC+05:00 (Pakistan Time)', offset: 'Etc/GMT-5' },
+      { name: 'UTC+06:00 (Bangladesh Time)', offset: 'Etc/GMT-6' },
+      { name: 'UTC+07:00 (Krasnoyarsk Time)', offset: 'Etc/GMT-7' },
+      { name: 'UTC+08:00 (China Standard Time)', offset: 'Etc/GMT-8' },
+      { name: 'UTC+09:00 (Japan Standard Time)', offset: 'Etc/GMT-9' },
+      { name: 'UTC+10:00 (Australian Western Standard Time)', offset: 'Etc/GMT-10' },
+      { name: 'UTC+11:00 (Solomon Islands Time)', offset: 'Etc/GMT-11' },
+      { name: 'UTC+12:00 (Kiribati Time)', offset: 'Etc/GMT-12' },
+      { name: 'UTC+13:00 (New Zealand Standard Time)', offset: 'Etc/GMT-13' },
+      { name: 'UTC+14:00 (Line Islands Time)', offset: 'Etc/GMT-14' },
+    ];
+    this.selectedTimeZone = this.timeZones[12];
   }
 
+  convertTime(): void {
+    let inputDate: Date;
+    if (this.inputEpoch !== null) {
+      inputDate = new Date(this.inputEpoch);
+    } else {
+      inputDate = new Date(this.inputDatetime);
+    }
+
+    if (isNaN(inputDate.getTime())) {
+      this.errorMessage = 'Invalid input. Please enter a valid date or epoch time.';
+      this.timeInfo = {};
+      return;
+    }
+    this.errorMessage = '';
+    this.timeInfo = {
+      Local: this.getFullTimeInfo(inputDate, Intl.DateTimeFormat().resolvedOptions().timeZone),
+      GMT: this.getFullTimeInfo(inputDate, 'GMT'),
+      Selected: this.getFullTimeInfo(inputDate, this.selectedTimeZone.offset)
+    };
+  }
+
+  private getFullTimeInfo(date: Date, timezone: string): any {
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: timezone,
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    };
+
+    const formattedDate = date.toLocaleString('en-US', options);
+    const parts = formattedDate.split(', ');
+    const dateParts = parts[0].split(' ');
+    const timeParts = parts[1].split(':');
+
+    return {
+      year: dateParts[2],
+      month: dateParts[0],
+      day: dateParts[1],
+      hour: timeParts[0],
+      minute: timeParts[1],
+      second: timeParts[2],
+      formattedDate: formattedDate,
+    };
+  }
+
+  protected readonly Object = Object;
 }
