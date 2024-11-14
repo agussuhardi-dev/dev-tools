a2410502792af591c511f3928a429d2ff944beb1
agussuhardi <agussuhardi.dev@gmail.com>
2024-11-14 22:15:33 +0700

    regex test

--- src/app/routes/text/regex-tester/regex-tester.component.ts
+++ src/app/routes/text/regex-tester/regex-tester.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { PageHeaderComponent } from '@shared';
+import { BreadcrumbComponent, PageHeaderComponent } from '@shared';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe, NgClass, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-text-regex-tester',
   templateUrl: './regex-tester.component.html',
   styleUrl: './regex-tester.component.scss',
   standalone: true,
-  imports: [PageHeaderComponent]
+  imports: [
+    BreadcrumbComponent,
+    PageHeaderComponent,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FormsModule,
+    NgIf,
+    NgForOf,
+    NgClass,
+    JsonPipe,
+  ],
 })
-export class TextRegexTesterComponent implements OnInit {
+export class TextRegexTesterComponent {
+  regexPattern: string = '';
+  testString: string = '';
+  flags: string = '';
+  matchResult: RegExpMatchArray | null = null;
+  isValid: boolean = true;
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) {}
 
-  ngOnInit() {
+  testRegex() {
+    try {
+      const regex = new RegExp(this.regexPattern, this.flags);
+      this.matchResult = this.testString.match(regex);
+      this.isValid = true;
+    } catch (error) {
+      this.isValid = false;
+      this.matchResult = null;
+      this.snackBar.open('Invalid regex pattern', 'Close', { duration: 3000 });
+    }
   }
-
 }
