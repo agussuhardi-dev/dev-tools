a5a3b5120e8d2765774a68a337da742fdca4a9a1
agussuhardi <agussuhardi.dev@gmail.com>
2024-11-14 23:30:21 +0700

    build electron

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: false
+    }
+  });
+
+  // Use loadFile instead of loadURL
+  win.loadFile(path.join(__dirname, 'dist/dev-tools/browser/index.html'));
+
+  // Open DevTools automatically
+  win.webContents.openDevTools();
+
+  win.setAutoHideMenuBar(true);
+  win.setMenuBarVisibility(false);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
